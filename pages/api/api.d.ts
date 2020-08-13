121a4cb7d537350bad85339d67f0773f6949b0e1
Brad Dunn <brad@braddunn.com>
2020-08-13 16:45:47 -0400

    Complete reading api

--- pages/api/api.d.ts
+++ pages/api/api.d.ts
@@ -3,3 +3,11 @@ type ApiResponse<T = object> = {
   message?: string;
   data?: T;
 };
+
+enum RequestMethod {
+  POST = "POST",
+  GET = "GET",
+  PUT = "PUT",
+  PATCH = "PATCH",
+  DELETE = "DELETE",
+}
--- pages/api/readings/index.ts
+++ pages/api/readings/index.ts
@@ -3,14 +3,46 @@ import { PrismaClient, Reading } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-type Data = {
+type GetResponse = {
   readings: Reading[];
 };
 
+type PostResponse = {
+  reading: Reading;
+};
+
+const createReading = async (value: string | string[]): Promise<Reading> => {
+  if (Array.isArray(value)) {
+    throw "An array of values is not valid. Pass in an single string instead";
+  }
+  const parsed = parseFloat(value);
+  if (isNaN(parsed)) {
+    throw "Invalid argument. Must pass in a number.";
+  }
+  const newReading = await prisma.reading.create({
+    data: { value: parsed },
+  });
+  return newReading;
+};
+
 export default async (
-  _req: NextApiRequest,
-  res: NextApiResponse<ApiResponse<Data>>
-) => {
-  const readings = await prisma.reading.findMany();
-  res.status(200).json({ status: 200, data: { readings } });
+  req: NextApiRequest,
+  res: NextApiResponse<ApiResponse<GetResponse | PostResponse>>
+): Promise<void> => {
+  try {
+    switch (req.method) {
+      case RequestMethod.POST:
+        const reading = await createReading(req.query.value);
+        res.status(200).json({ status: 200, data: { reading } });
+        break;
+      case RequestMethod.GET:
+        const readings = await prisma.reading.findMany();
+        res.status(200).json({ status: 200, data: { readings } });
+        break;
+      default:
+        throw "Bad request.";
+    }
+  } catch (error) {
+    res.status(400).json({ status: 400, message: error.message });
+  }
 };
